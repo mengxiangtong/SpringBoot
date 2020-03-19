728b67b478036621bcff3615c9c988de1c2d4045
mengxiangtong <928593829@qq.com>
2020-03-19 22:28:13 +0800

    06---mybatis--add

---
+++ SpringBoot/spring-boot-06-data-mybatis/src/main/java/com/wdjr/springboot/service/CourseService.java
@@ -0,0 +1,12 @@
+package com.wdjr.springboot.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CourseService {
+
+
+
+
+}
---
+++ SpringBoot/spring-boot-06-data-mybatis/src/main/java/com/wdjr/springboot/service/CourseServiceImpl.java
@@ -0,0 +1,13 @@
+package com.wdjr.springboot.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CourseServiceImpl implements  CourseService  {
+
+@Override
+public void addCourse(C)
+
+
+}
